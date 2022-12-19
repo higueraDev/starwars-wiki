fddf4713cea12fd0f34019188072ae2053513be2
higueraDev <davidhigueralp@hotmail.com>
2022-12-18 22:49:01 -0700

    [ADDED]:Logo Component

---
+++ src/app/components/common/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss'],
+})
+export class LogoComponent {
+  public title = 'Star Wars';
+  public subtitle = 'Wiki';
+}
