b2c5d76999279a3c0c5b43e52370a1fe6a894d3c
higueraDev <davidhigueralp@hotmail.com>
2022-12-19 12:00:58 -0700

    [UPDATED]: Logo overlay

--- src/app/components/common/logo/logo.component.ts
+++ src/app/components/common/logo/logo.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { StoreService } from '../../../services/store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent {
+export class LogoComponent implements OnDestroy {
+  public overlayOpacity: number = 0;
   public title = 'Star Wars';
   public subtitle = 'Wiki';
+  scrolledSubscription: Subscription | null = null;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit() {
+    this.scrolledSubscription = this.storeService.readIsScrolled$.subscribe(
+      (value) => {
+        this.overlayOpacity = value ? 1 : 0;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.scrolledSubscription?.unsubscribe();
+  }
 }
