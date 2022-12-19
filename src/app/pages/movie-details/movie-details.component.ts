4825435e439d75d9db9bd9468da38c12edf95982
higueraDev <davidhigueralp@hotmail.com>
2022-12-18 22:53:06 -0700

    [ADDED]: Movie Details styles

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,17 +1,63 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Film } from '../../models/entities/film';
+import { FilmsService } from '../../services/films.service';
+import { InterfaceFactoryService } from '../../services/interface-factory.service';
+import { FilmDto } from '../../models/dtos/film-dto';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
 })
-export class MovieDetailsComponent {
-  @Input() film!: Film;
+export class MovieDetailsComponent implements OnInit, OnDestroy {
+  public film: Film | null = null;
   public image: string = '';
+  public filmNumber: string = '';
+  private routerSubscription: Subscription | null = null;
+  private filmSubscription: Subscription | null = null;
 
-  ngOnChanges() {
-    if (this.image === '' && this.film.episode_id)
+  constructor(
+    private route: ActivatedRoute,
+    private filmsService: FilmsService,
+    private interfaceFactoryService: InterfaceFactoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.readFilmNumber();
+    this.getFilm();
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription?.unsubscribe();
+    this.filmSubscription?.unsubscribe();
+  }
+
+  loadImage() {
+    if (this.image === '' && this.film)
       this.image = `./assets/images/films/EP${this.film.episode_id}_POS.jpg`;
+  }
+
+  readFilmNumber() {
+    this.routerSubscription = this.route.paramMap.subscribe((params) => {
+      this.filmNumber = params.get('filmNumber') || '';
+    });
+  }
+
+  getFilm() {
+    this.filmSubscription = this.filmsService
+      .getFilms(this.filmNumber)
+      .subscribe((data) => {
+        const isOneFilm = this.interfaceFactoryService.checkInterface<FilmDto>(
+          data,
+          'episode_id'
+        );
+
+        if (isOneFilm && data) {
+          this.film = data;
+          this.loadImage();
+        }
+      });
   }
 }
