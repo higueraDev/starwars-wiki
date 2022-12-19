53f2e86145bc639763ef8c20fc74f6aa2e573977
higueraDev <davidhigueralp@hotmail.com>
2022-12-18 22:47:45 -0700

    [ADDED] Routes features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movie-details/:filmNumber',
+    component: MovieDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/common/film/film.component.ts
+++ src/app/components/common/film/film.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { Film } from '../../../models/entities/film';
+import { StoreService } from '../../../services/store.service';
 
 @Component({
   selector: 'app-film',
@@ -9,9 +11,17 @@ import { Film } from '../../../models/entities/film';
 export class FilmComponent implements OnChanges {
   @Input() film!: Film;
   public image: string = '';
+  public filmNumber: string = '';
+
+  constructor(private storeService: StoreService, private router: Router) {}
 
   ngOnChanges() {
     if (this.image === '' && this.film.episode_id)
       this.image = `./assets/images/films/EP${this.film.episode_id}_POS.jpg`;
+  }
+
+  onClick() {
+    this.filmNumber = this.storeService.getFilmNumber(this.film.episode_id);
+    this.router.navigate(['/movie-details', this.filmNumber]);
   }
 }
