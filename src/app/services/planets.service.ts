4fb2029b79941b14c9d7b74becc42b5c0301512f
higueraDev <davidhigueralp@hotmail.com>
2022-12-19 18:09:23 -0700

    [UPDATED]: Planet Service type

--- src/app/services/planets.service.ts
+++ src/app/services/planets.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs';
+import { PlanetDto } from '../models/dtos/planet-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class PlanetsService {
   constructor(private http: HttpClient) {}
 
   getPlanet(url: string) {
-    return this.http.get(url).pipe(take(1));
+    return this.http.get<PlanetDto>(url).pipe(take(1));
   }
 }
